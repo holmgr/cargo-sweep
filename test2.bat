2cb83ca27c1d36d344560ca1c3e2f459a81b07c3
Finkelman <finkelman@semcog.org>
2019-01-06 18:28:43 -0500

    it is called cargo-sweep not hello

---
+++ test2.bat
@@ -0,0 +1,12 @@
+set RUSTC=C:\Users\finkelman.SEMCOGDOM\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\bin\rustc.exe
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" build
+set RUSTC=C:\Users\finkelman.SEMCOGDOM\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin\rustc.exe
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" build
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" sweep -s
+set RUSTC=C:\Users\finkelman.SEMCOGDOM\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin\rustc.exe
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" build
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" sweep -f
+set RUSTC=C:\Users\finkelman.SEMCOGDOM\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin\rustc.exe
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" build
+set RUSTC=C:\Users\finkelman.SEMCOGDOM\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\bin\rustc.exe
+"C:\Users\finkelman.SEMCOGDOM\Documents\MyProjects\cargo\target\debug\cargo.exe" build
