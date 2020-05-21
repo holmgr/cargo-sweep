holmgr/cargo-sweep

commit e53601a2a2212cb4428e88c4db6ea7c41150cedc

--time option requires a value
Remove help text incorrectly indicating that it might not

src/main.rs
@@ -170,7 +170,7 @@ fn main() {
                         .short("t")
                         .long("time")
                         .value_name("days")
-                        .help("Number of days backwards to keep. If no value is set uses 30.")
+                        .help("Number of days backwards to keep")
                         .takes_value(true),
                 )
                 .group(
@@ -205,7 +205,6 @@ fn main() {
         } else {
             let days_to_keep: u64 = matches
                 .value_of("time")
-                .unwrap_or("30")
                 .parse()
                 .expect("Invalid time format");
             Duration::from_secs(days_to_keep * 24 * 3600)

commit 4430478f20561aa8d9fde07441a5bf2829605aab

Fix missing unwrap (broken in e53601a2)

src/main.rs
@@ -205,6 +205,7 @@ fn main() {
         } else {
             let days_to_keep: u64 = matches
                 .value_of("time")
+                .expect("--time argument missing")
                 .parse()
                 .expect("Invalid time format");
             Duration::from_secs(days_to_keep * 24 * 3600)