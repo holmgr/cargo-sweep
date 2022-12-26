fd4dc55dd89d480276aace44b1af6adeeab7ed05
João Marcos Bezerra <marcospb19@hotmail.com>
2022-12-26 18:53:55 -0300

    Make default output less noisy (#81)
    * Make default output less noisy
    
    by logging the list of deleted files on the 'Debug' log level, requiring
    the user to provide the --verbose flag to see it
    
    previously, this list was logged to the 'Info' log level
    
    * Show the path of the cleaned project
    
    * refactor test function `run`
    
    now it requires a target directory to which cargo-sweep will try to
    perform the cleaning
    
    * add test to compare new default output

--- tests/integration.rs
+++ tests/integration.rs
@@ -16,7 +16,9 @@ use predicates::{
 };
 #[allow(unused_imports)]
 use pretty_assertions::{assert_eq, assert_ne};
+use regex::Regex;
 use tempfile::{tempdir, TempDir};
+use unindent::unindent;
 
 struct AnyhowWithContext(anyhow::Error);
 impl Debug for AnyhowWithContext {
@@ -49,11 +51,16 @@ fn sweep(args: &[&str]) -> Command {
     let mut cmd = Command::new(cargo_bin("cargo-sweep"));
     cmd.arg("sweep")
         .current_dir(project_dir("sample-project"))
+        .arg("--verbose")
         .args(args);
     cmd
 }
 
-fn run(mut cmd: impl BorrowMut<Command>) -> Assert {
+/// Sets the target folder and runs the given command
+fn run<'a>(mut cmd: impl BorrowMut<Command>, target: impl Into<Option<&'a Path>>) -> Assert {
+    if let Some(target) = target.into() {
+        cmd.borrow_mut().env("CARGO_TARGET_DIR", target);
+    }
     let assert = cmd.borrow_mut().assert().success();
     let out = assert.get_output();
     let str = |s| std::str::from_utf8(s).unwrap();
@@ -66,9 +73,7 @@ fn run(mut cmd: impl BorrowMut<Command>) -> Assert {
 fn build(project: &str) -> Result<(u64, TempDir)> {
     let target = tempdir()?;
     let old_size = get_size(target.path())?;
-    run(cargo(project)
-        .arg("build")
-        .env("CARGO_TARGET_DIR", target.path()));
+    run(cargo(project).arg("build"), target.path());
     let new_size = get_size(target.path())?;
     assert!(new_size > old_size, "cargo didn't build anything");
     Ok((new_size, target))
@@ -82,18 +87,26 @@ fn clean_and_parse(target: &TempDir, args: &[&str]) -> Result<u64> {
     } else {
         ("Successfully removed", "Cleaned ")
     };
-    let assertion = run(sweep(args).env("CARGO_TARGET_DIR", target.path()))
-        .stdout(contains(remove_msg).and(contains(clean_msg)));
+    let assertion =
+        run(sweep(args), target.path()).stdout(contains(remove_msg).and(contains(clean_msg)));
 
     let output = assertion.get_output();
     assert!(output.stderr.is_empty());
+
+    // Extract the size from the last line of stdout, example:
+    // - stdout: "[INFO] Would clean: 9.43 KiB from "/home/user/project/target"
+    // - extracted amount: "9.43 KiB "
     let amount = std::str::from_utf8(&output.stdout)?
         .lines()
         .last()
         .unwrap()
         .split(clean_msg)
         .nth(1)
-        .unwrap();
+        .unwrap()
+        .split_inclusive(' ')
+        .take(2)
+        .collect::<String>();
+
     let cleaned = amount
         .parse::<human_size::Size>()
         .context(format!("failed to parse amount {amount}"))?
@@ -114,7 +127,7 @@ fn count_cleaned(target: &TempDir, args: &[&str], old_size: u64) -> Result<u64> 
     let one_percent = old_size as f64 * 0.01;
     assert!(
         diff <= one_percent as u64,
-        "new_size={new_size}, old_size={old_size}, cleaned={cleaned}, diff={diff}, 1%={one_percent}"
+        "new_size={new_size}, old_size={old_size}, cleaned={cleaned}, diff={diff}, 1%={one_percent}",
     );
 
     Ok(cleaned)
@@ -129,6 +142,11 @@ fn count_cleaned_dry_run(target: &TempDir, args: &[&str], old_size: u64) -> Resu
     assert_eq!(old_size, new_size);
 
     Ok(cleaned)
+}
+
+fn regex_matches(pattern: &str, text: &str) -> bool {
+    let pattern = Regex::new(pattern).expect("Failed to compile regex pattern");
+    pattern.is_match(text)
 }
 
 #[test]
@@ -158,7 +176,7 @@ fn stamp_file() -> TestResult {
     let (size, target) = build("sample-project")?;
 
     // Create a stamp file for --file.
-    let assert = run(sweep(&["--stamp", "-v"]));
+    let assert = run(sweep(&["--stamp"]), target.path());
     println!(
         "{}",
         std::str::from_utf8(&assert.get_output().stdout).unwrap()
@@ -173,10 +191,47 @@ fn stamp_file() -> TestResult {
 
     // For some reason, we delete the stamp file after `--file` :(
     // Recreate it.
-    run(sweep(&["--stamp"]));
+    run(sweep(&["--stamp"]), target.path());
 
     let actual_cleaned = count_cleaned(&target, args, size)?;
     assert_eq!(actual_cleaned, expected_cleaned);
+
+    Ok(())
+}
+
+#[test]
+fn empty_project_output() -> TestResult {
+    let (_size, target) = build("sample-project")?;
+
+    let assert = run(
+        sweep(&["--maxsize", "0"]).current_dir(test_dir().join("sample-project")),
+        target.path(),
+    );
+
+    let output = std::str::from_utf8(&assert.get_output().stdout).unwrap();
+
+    let pattern = unindent(
+        r#"\[DEBUG\] cleaning: ".+" with remove_older_until_fits
+        \[DEBUG\] size_to_remove: .+
+        \[DEBUG\] Sizing: ".+debug" with total_disk_space_in_a_profile
+        \[DEBUG\] Hashs by time: \[
+            \(
+                .+,
+                ".+",
+            \),
+        \]
+        \[DEBUG\] cleaning: ".+debug" with remove_not_built_with_in_a_profile
+        \[DEBUG\] Successfully removed: ".+libsample_project-.+\.rlib"
+        \[DEBUG\] Successfully removed: ".+libsample_project-.+\.rmeta"
+        \[DEBUG\] Successfully removed: ".+sample_project-.+\.d"
+        \[DEBUG\] Successfully removed: ".+.fingerprint.+sample-project-.+"
+        \[INFO\] Cleaned .+ from ".+""#,
+    );
+
+    assert!(
+        regex_matches(&pattern, output),
+        "failed to match pattern with output"
+    );
 
     Ok(())
 }
@@ -187,9 +242,10 @@ fn hidden() -> TestResult {
     // So we can't let cargo-sweep discover any other projects, or it will think they share the same directory as this hidden project.
     let (size, target) = build("fresh-prefix/.hidden/hidden-project")?;
     let run = |args| {
-        run(sweep(args)
-            .current_dir(test_dir().join("fresh-prefix"))
-            .env("CARGO_TARGET_DIR", target.path()))
+        run(
+            sweep(args).current_dir(test_dir().join("fresh-prefix")),
+            target.path(),
+        )
     };
 
     run(&["--maxsize", "0", "-r"]);
@@ -221,9 +277,10 @@ fn error_output() -> TestResult {
     }
 
     let (_, tempdir) = build("sample-project")?;
-    let assert = run(sweep(&["--installed"])
-        .env("PATH", test_dir())
-        .env("CARGO_TARGET_DIR", tempdir.path()));
+    let assert = run(
+        sweep(&["--installed"]).env("PATH", test_dir()),
+        tempdir.path(),
+    );
     assert.stdout(contains("oh no an error"));
 
     Ok(())
@@ -241,7 +298,7 @@ fn error_status() -> TestResult {
 
 fn golden_reference(args: &[&str], file: &str) -> TestResult {
     let mut cmd = Command::new(cargo_bin("cargo-sweep"));
-    let mut assert = run(cmd.args(args));
+    let mut assert = run(cmd.args(args), None);
 
     assert = assert.stderr(is_empty());
     let actual = std::str::from_utf8(&assert.get_output().stdout)?;
@@ -264,9 +321,7 @@ fn path() -> TestResult {
     cmd.arg("sweep").arg("--installed").current_dir(temp_dir());
 
     // Pass `path` as an argument, instead of `current_dir` like it normally is.
-    let assert = run(cmd
-        .arg(project_dir("sample-project"))
-        .env("CARGO_TARGET_DIR", target.path()));
+    let assert = run(cmd.arg(project_dir("sample-project")), target.path());
     assert.stdout(contains("Cleaned"));
 
     Ok(())
